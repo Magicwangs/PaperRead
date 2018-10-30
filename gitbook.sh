90adce19d10c3bc644b742f5f5911f4851b59ee6
MagicWang <752106129@qq.com>
2018-10-30 10:31:35 +0800

    init

--- gitbook.sh
+++ gitbook.sh
@@ -1,5 +1,5 @@
 gitbook build
-git checkout master
+#git checkout master
 git add .
 git commit -m $1
 git push -u both master
@@ -8,5 +8,6 @@ cp -r _book/* .
 git add .
 git commit -m $1
 git push -u origin gh-pages
+git checkout coding
 git push -u coding coding-pages
 git checkout master
